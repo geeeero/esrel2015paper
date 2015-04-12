8c7a27532c553cbbe0d78101bbbfba0635982844
Gero Walter <g.m.walter@tue.nl>
2015-04-13 01:09:27 +0200

    IG variances, fig1

--- figure1.R
+++ figure1.R
@@ -14,7 +14,7 @@ lambdatofailure <- function(lambda, k = 2){
 # translate failure time to lambda parameter of weibull
 # ft - expected failure time
 # k  - shape parameter of weibull
-failuretolambda <- function(ft, k){
+failuretolambda <- function(ft, k = 2){
   (ft/gamma(1 + 1/k))^k
 }
 
@@ -27,7 +27,46 @@ t2 <- 2
 
 n2 <- n0 + 2
 y2 <- (n0*y0 + t1^k + t2^k)/n2
+y2
 
 eft2 <- lambdatofailure(y2,k)
+eft2
+
+invgammavar <- function(n0, y0)
+  y0^2/(1-1/n0)
+
+invgammavar(n2, y2)
+
+# provides dinvgamma(x, shape, scale) where shape = alpha and scale = beta
+library(actuar)
+
+# inverse gamma pdf and cdf with canonical parameters n0 and y0
+cdinvgamma <- function(x, n0, y0, ...)
+  dinvgamma(x, shape = n0+1, scale = n0*y0, ...)
+
+cpinvgamma <- function(x, n0, y0, ...)
+  pinvgamma(x, shape = n0+1, scale = n0*y0, ...)
+
+xseq <- seq(0.1, 200, length.out = 300)
+
+par(mar=c(3,3,1,1)+0.1)
+plot(xseq, cdinvgamma(xseq, n2, y2), type="l", xlab="", ylab="")
+mtext(expression(lambda), 1, 2)
+mtext(expression(p(lambda)), 2, 2)
+lines(xseq, cdinvgamma(xseq, n0, y0), lty=2)
+legend("topright", legend = c("prior", "posterior"), lty = c(2,1),
+       inset = 0.02)
+#title(main = "Prior and posterior densities")
+
+setEPS()
+postscript("fig1.eps",width=5,height=3)
+par(mar=c(3,3,1,1)+0.1)
+plot(xseq, cpinvgamma(xseq, n2, y2), type="l", xlab="", ylab="")
+mtext(expression(lambda), 1, 2)
+mtext(expression(p(lambda)), 2, 2)
+lines(xseq, cpinvgamma(xseq, n0, y0), lty=2)
+legend("bottomright", legend = c("prior", "posterior"), lty = c(2,1),
+       inset = 0.02)
+dev.off()
 
 #
