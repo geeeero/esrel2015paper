fede7b36da7b4e0897684d6973d86ef582bd2c15
Gero Walter <g.m.walter@tue.nl>
2015-07-16 13:49:41 +0200

    add option prior to all functions

--- notes-isipta15poster.R
+++ notes-isipta15poster.R
@@ -40,25 +40,34 @@ cpinvgamma <- function(x, n0, y0, ...)
 # fts     vector of length e giving the observed failure times,
 #         or NULL if no failures observed
 # kappa   fixed weibull shape parameter
-postpredC <- function(n0y0, kappa, n, fts, tnow, t, l){
+# prior   whether the prior predictive should be calculated,
+#         when FALSE, fts is ignored 
+postpredC <- function(n0y0, kappa, n, fts, tnow, t, l, prior = FALSE){
   if (t < tnow)
     stop("t must be larger than tnow")
-  e <- length(fts)
-  if (n < e)
-    stop("there can be at most n failure times!")
+  if(!prior){
+    e <- length(fts)
+    if (n < e)
+      stop("there can be at most n failure times!")
+  } else {
+    e <- 0
+  }
   if (l > n-e)
     stop("l can be at most n-e")
   nn <- n0y0[1] + e
-  nnyn <- n0y0[1]*n0y0[2] + (n-e)*tnow + sum(fts^kappa)
+  if(!prior)
+    nnyn <- n0y0[1]*n0y0[2] + (n-e)*tnow + sum(fts^kappa)
+  else
+    nnyn <- n0y0[1]*n0y0[2]
   j <- seq(0, n-e-l)
   choose(n-e, l) * sum( (-1)^j * choose(n-e-l, j) * (nnyn/(nnyn + (l+j)*(t^kappa - tnow^kappa)))^(nn + 1))
 }
 
 # calculates the probability mass function for C_t
-postpredCpmf <- function(n0y0, kappa, n, fts, tnow, t){
+postpredCpmf <- function(n0y0, kappa, n, fts, tnow, t, prior = FALSE){
   l <- seq(0, n-length(fts))
   res <- numeric(length(l))
-  for (i in l) res[i+1] <- postpredC(n0y0, kappa, n, fts, tnow, t, i)
+  for (i in l) res[i+1] <- postpredC(n0y0, kappa, n, fts, tnow, t, i, prior = prior)
   res <- array(res)
   dimnames(res)[[1]] <- l
   res
@@ -68,8 +77,8 @@ postpredCpmf <- function(n0y0, kappa, n, fts, tnow, t){
 
 
 # calculates the cumulative mass function for C_t
-postpredCcmf <- function(n0y0, kappa, n, fts, tnow, t){
-  pmf <- postpredCpmf(n0y0, kappa, n, fts, tnow, t)
+postpredCcmf <- function(n0y0, kappa, n, fts, tnow, t, prior = FALSE){
+  pmf <- postpredCpmf(n0y0, kappa, n, fts, tnow, t, prior = prior)
   cmf <- cumsum(pmf)
   cmf
 }
@@ -84,13 +93,13 @@ Ccmfplot <- function(cmf, add = FALSE, ylim = c(0,1), xlab = "l", ylab = "F(C = 
 
 
 # plots and prints the cmfs for the four 'corner' (n0,y0) pairs.
-fourCornersCcmf <- function(luckobj, kappa, n, fts, tnow, t){
+fourCornersCcmf <- function(luckobj, kappa, n, fts, tnow, t, prior = FALSE){
   n0 <- n0(luckobj)
   y0 <- y0(luckobj)
-  tl <- postpredCcmf(n0y0 = c(n0[1], y0[2]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t)
-  tr <- postpredCcmf(n0y0 = c(n0[2], y0[2]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t)
-  bl <- postpredCcmf(n0y0 = c(n0[1], y0[1]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t)
-  br <- postpredCcmf(n0y0 = c(n0[2], y0[1]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t)
+  tl <- postpredCcmf(n0y0 = c(n0[1], y0[2]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t, prior = prior)
+  tr <- postpredCcmf(n0y0 = c(n0[2], y0[2]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t, prior = prior)
+  bl <- postpredCcmf(n0y0 = c(n0[1], y0[1]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t, prior = prior)
+  br <- postpredCcmf(n0y0 = c(n0[2], y0[1]), kappa = kappa, n = n, fts = fts, tnow = tnow, t = t, prior = prior)
   Ccmfplot(tl, main = bquote(paste("n0 = [",.(n0[1]),",",.(n0[2]),"], y0 = [",.(round(y0[1],2)),",",.(round(y0[2],2)),"]")))
   Ccmfplot(tr, lty = 2, add = TRUE)
   Ccmfplot(bl, col = 2, add = TRUE)
@@ -122,13 +131,16 @@ fourCornersCcmf <- function(luckobj, kappa, n, fts, tnow, t){
 # table    if results table should be given along with the reliability
 # nk       vector of length K giving the number of components of each type,
 #          only needed when reduced survival signature is used
-sysrel <- function(n0y0, survsign, kappa, fts, tnow, t, table = FALSE, nk = NULL){
+sysrel <- function(n0y0, survsign, kappa, fts, tnow, t, table = FALSE, nk = NULL, prior = FALSE){
   K <- length(fts)
   if(is.null(nk)){
     nk <- apply(survsign, 2, max)
     nk <- nk[-length(nk)]
   }
-  ek <- unlist(lapply(fts, length))
+  if(!prior)
+    ek <- unlist(lapply(fts, length))
+  else
+    ek <- rep(0, K)
   # sumarray <- array(data = NA, dim = nk-ek+1)
   # dimnames(sumarray) <- lapply(as.list(nk - ek), function(x) as.character(seq(0,x)))
   # signarray <- sumarray
@@ -144,7 +156,7 @@ sysrel <- function(n0y0, survsign, kappa, fts, tnow, t, table = FALSE, nk = NULL
     names(survsign2) <- c(names(survsign2)[-ncols], newname)
     for (i in 1:dim(survsign2)[1]){
       survsign2[i,ncols] <- postpredC(n0y0 = n0y0[[k]], kappa = kappa[k], n = nk[k],
-                                      fts = fts[[k]], tnow = tnow, t = t, l = survsign2[i,k])
+                                      fts = fts[[k]], tnow = tnow, t = t, l = survsign2[i,k], prior = prior)
     }
   }
   survsign2$summand <- apply(survsign2[,-(1:K)], 1, prod)
@@ -164,7 +176,7 @@ sysrel <- function(n0y0, survsign, kappa, fts, tnow, t, table = FALSE, nk = NULL
 #             the list element should be NULL if no failure has been observed for type k, 
 # tnow        time until the system is observed
 # tvec        vector of timepoints for which to calculate P(T_sys > t), t > t_now
-fourKcornersSysrel <- function(luckobjlist, survsign, kappa, fts, tnow, tvec, nk = NULL){
+fourKcornersSysrel <- function(luckobjlist, survsign, kappa, fts, tnow, tvec, nk = NULL, prior = FALSE){
   K <- length(luckobjlist)
   ncomb <- 2^K
   combs <- as.list(rep(NA,K))
@@ -181,7 +193,7 @@ fourKcornersSysrel <- function(luckobjlist, survsign, kappa, fts, tnow, tvec, nk
     for (k in 1:K)    # over rows!
       comblist[[k]] <- c(n0(luckobjlist[[k]])[lowercorners[k,i]], y0(luckobjlist[[k]])[1])
     rvec <- sapply(tvec, FUN = sysrel, n0y0 = comblist,
-                   survsign = survsign, kappa=kappa, fts = fts, tnow = tnow, nk = nk)
+                   survsign = survsign, kappa=kappa, fts = fts, tnow = tnow, nk = nk, prior = prior)
     lowercorners[(1:length(rvec))+K,i] <- rvec
   }
   # all n0 combinations with upper y's
@@ -190,7 +202,7 @@ fourKcornersSysrel <- function(luckobjlist, survsign, kappa, fts, tnow, tvec, nk
     for (k in 1:K)    # over rows!
       comblist[[k]] <- c(n0(luckobjlist[[k]])[lowercorners[k,i]], y0(luckobjlist[[k]])[2])
     rvec <- sapply(tvec, FUN = sysrel, n0y0 = comblist,
-                   survsign = survsign, kappa=kappa, fts = fts, tnow = tnow, nk = nk)
+                   survsign = survsign, kappa=kappa, fts = fts, tnow = tnow, nk = nk, prior = prior)
     uppercorners[(1:length(rvec))+K,i] <- rvec
   }
   list(lower = lowercorners, upper = uppercorners)
@@ -236,15 +248,15 @@ fourKcornersSysrelPlot <- function(tvec, rframe, legend = FALSE, add = FALSE,
 # tnow        time until the system is observed
 # t           timepoint for which to calculate P(T_sys > t), t > t_now
 # returnasvec if output should be a vector (lower bound, upper bound, l.b. parameters, u.b. parameters)
-sysrelLuck <- function(luckobjlist, survsign, kappa, fts, tnow, t, returnasvec = FALSE, nk = NULL){
+sysrelLuck <- function(luckobjlist, survsign, kappa, fts, tnow, t, returnasvec = FALSE, nk = NULL, prior = FALSE){
   K <- length(luckobjlist)
-  optimfu <- function(n0vec, y0vec, K, survsign, kappa, fts, tnow, t, nk){
+  optimfu <- function(n0vec, y0vec, K, survsign, kappa, fts, tnow, t, nk, prior = prior){
     n0y0 <- as.list(rep(NA, K))
     for (k in 1:K)
       n0y0[[k]] <- c(n0vec[k], y0vec[k])
     #cat("optimfu called with n0y0vec:\n")
     #print(n0y0)
-    sysrel(n0y0 = n0y0, survsign = survsign, kappa = kappa, fts = fts, tnow = tnow, t = t, nk = nk)
+    sysrel(n0y0 = n0y0, survsign = survsign, kappa = kappa, fts = fts, tnow = tnow, t = t, nk = nk, prior = prior)
   }
   parl <- numeric(K)
   paru <- numeric(K)
@@ -256,9 +268,9 @@ sysrelLuck <- function(luckobjlist, survsign, kappa, fts, tnow, t, returnasvec =
     y0u[k] <- y0(luckobjlist[[k]])[2]
   }
   optl <- optim(par = parl, fn = optimfu, method = "L-BFGS-B", lower = parl, upper = paru,
-                y0vec = y0l, K = K, survsign = survsign, kappa = kappa, fts = fts, tnow = tnow, t = t, nk = nk)
+                y0vec = y0l, K = K, survsign = survsign, kappa = kappa, fts = fts, tnow = tnow, t = t, nk = nk, prior = prior)
   optu <- optim(par = paru, fn = optimfu, method = "L-BFGS-B", lower = parl, upper = paru,
-                y0vec = y0u, K = K, survsign = survsign, kappa = kappa, fts = fts, tnow = tnow, t = t, nk = nk,
+                y0vec = y0u, K = K, survsign = survsign, kappa = kappa, fts = fts, tnow = tnow, t = t, nk = nk, prior = prior,
                 control = list(fnscale = -1))
   if(returnasvec)
     return(c(optl$value, optu$value, optl$par, optu$par))
@@ -279,11 +291,12 @@ sysrelLuck <- function(luckobjlist, survsign, kappa, fts, tnow, t, returnasvec =
 # returnres   if a data frame containing the lower and upper bound, together with the n's for
 #             which these bounds are obtained
 # noplot      whether no plot should should be produced (sets returnres = TRUE)
-sysrelPbox <- function(luckobjlist, survsign, kappa, fts, tnow, tvec, nk = NULL, add = FALSE, ylim = c(0,1),
-                       xlab = "t", ylab = expression(R[sys](t)), polygonBorderCol = NA, polygonFillCol = "grey",
+sysrelPbox <- function(luckobjlist, survsign, kappa, fts, tnow, tvec, nk = NULL, prior = prior,
+                       add = FALSE, ylim = c(0,1), xlab = "t", ylab = expression(R[sys](t)),
+                       polygonBorderCol = NA, polygonFillCol = "grey",
                        returnres = FALSE, noplot = FALSE, ...){
   res <- sapply(tvec, FUN = sysrelLuck, luckobjlist = luckobjlist, survsign = survsign,
-                kappa = kappa, fts = fts, tnow = tnow, nk = nk, returnasvec = TRUE)
+                kappa = kappa, fts = fts, tnow = tnow, nk = nk, prior = prior, returnasvec = TRUE)
   res <- t(res)
   K <- length(luckobjlist)
   colnames(res) <- c("lower", "upper", paste("ln", 1:K, sep=""), paste("un", 1:K, sep=""))
